2124e7e4b54c349b30aa361b60066f92b931dc66
Benjamin Lee <benjamindlee@me.com>
2020-02-05 23:09:15 -0500

    Change the name of the component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
     <Navbar />
-    <ContactsTable />
+    <ContactsList />
     <ContactEntryModal id="henlo" />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
-import ContactsTable from "./components/ContactsTable";
+import ContactsList from "./components/ContactsList";
 import ContactEntryModal from "./components/ContactEntryModal";
 
 export default {
   name: "app",
   components: {
     Navbar,
-    ContactsTable,
+    ContactsList,
     ContactEntryModal,
   },
   mounted() {
