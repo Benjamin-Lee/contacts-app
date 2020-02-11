63a046b9edc627ae7fad96de53dc25fbe4702c61
Benjamin Lee <benjamindlee@me.com>
2020-02-10 19:43:54 -0500

    Fix broken address display

--- src/components/ContactDisplayModal.vue
+++ src/components/ContactDisplayModal.vue
@@ -19,16 +19,14 @@
         </a>
       </dd>
 
-      <template v-if="address[0].line1 !== ''">
-        <dt class="col-sm-2">Address</dt>
-        <dd class="col-sm-10">
-          <p class="my-0">
-            {{ address[0].line1 }},
-            {{ address[0].line2 ? address[0].line2 + "," : "" }}
-            {{ address[0].city }} {{ address[0].zip }}, {{ address[0].state }}
-          </p>
-        </dd>
-      </template>
+      <dt class="col-sm-2" v-if="address[0].line1 !== ''">Address</dt>
+      <dd class="col-sm-10" v-if="address[0].line1 !== ''">
+        <p class="my-0" v-for="(_address, index) in address" :key="index">
+          {{ _address.line1 }},
+          {{ _address.line2 ? _address.line2 + "," : "" }}
+          {{ _address.city }} {{ _address.zip }}, {{ _address.state }}
+        </p>
+      </dd>
     </dl>
   </div>
 </template>
